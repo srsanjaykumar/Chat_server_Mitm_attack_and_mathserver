96ce1d2bc3213dd74fd4b9da3f047600c491f89a
srsanjaykumar <mesrsanjay@gmail.com>
2023-06-26 20:43:28 +0530

    socket connection making

--- math_bc/multithreaded_math_server.py
+++ math_bc/multithreaded_math_server.py
@@ -15,8 +15,25 @@ class ProcessOutputThread(Thread):
         while not self.process.stdout:
             self.conn.sendall(self.process.stdout.readline())
 
+
+
+#initilize socket 
+
+s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.bind((HOST,PORT))
+s.listen()
+addr,conn=s.accept()
+#Connection and port forwarding 
+print("{} Connecting with Back Port => {} ".format(addr[0],addr[1]))
+print("You are Connected into Math Server . Please give some Math Expressions .....\n\n :> $\t")
 # get a bc command 
-p=Popen(['bc'],stderr=STDOUT,stdin=PIPE,stdout=PIPE)
-t=Thread(target=ProcessOutputThread,args=(p,))
+p=Popen(['bc'],stderr=STDOUT,stdin=PIPE,stdout=PIPE,shell=True)
+t=Thread(target=ProcessOutputThread,args=(p,conn))
 t.start()
+
+
+
+
+
 
