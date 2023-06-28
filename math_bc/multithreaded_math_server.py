6108c06ccece7acd8d147bc5ed4ad5007610684a
srsanjaykumar <mesrsanjay@gmail.com>
2023-06-28 09:01:18 +0530

    general coonnection making and exit , quit not fixed in this stage

--- math_bc/multithreaded_math_server.py
+++ math_bc/multithreaded_math_server.py
@@ -4,6 +4,11 @@ from subprocess import PIPE , STDOUT ,Popen
 
 HOST=""
 PORT=7070
+
+def Start_new_math_thread(conn,addr):
+    t=MathServerCommunicationThread(conn,addr)
+    t.start()
+
 # run a stdout in seperate thread 
 class ProcessOutputThread(Thread):
     def __init__(self,process,conn):
@@ -12,23 +17,41 @@ class ProcessOutputThread(Thread):
         self.conn=conn
     
     def run(self):
-        while not self.process.stdout:
+        while not self.process.stdout.closed:
             self.conn.sendall(self.process.stdout.readline())
 
 
 class MathServerCommunicationThread(Thread):
-    def __init__(self,process,conn):
+    def __init__(self,conn,addr):
         Thread.__init__(self)
-        self.process=process
+        self.addr=addr
         self.conn=conn
-    def run():
+    def run(self):
         #Connection and port forwarding 
         print("{} Connecting with Back Port => {}".format(addr[0],addr[1]))
-        conn.sendall("You are Connected into Math Server . Please give some Math Expressions .....\n\n :> $\t".encode())
+        conn.sendall("You are Connected into Math Server . Please give some Math Expressions .....\n\n$".encode())
         # get a bc command 
         p=Popen(['bc'],stderr=STDOUT,stdin=PIPE,stdout=PIPE,shell=True)
-        t=Thread(target=ProcessOutputThread,args=(p,conn))
-        t.start()
+        # call classes ini thread in proper way 
+        out=ProcessOutputThread(p,conn)
+        out.start()
+
+        while not p.stdout.closed:
+            data = conn.recv(1024)
+            if not data:
+                break
+            else:
+                data=data.decode()
+                query=data.strip()
+                if(query =="exit" or query == "quit"):
+                    p.communicate(query.encode(),timeout=1)
+                    if p.poll() is not None:
+                        break
+                query = query + "\n"
+                p.stdin.write(query.encode())
+                p.stdin.flush()
+        s.close()
+
 
 #initilize socket 
 
@@ -38,8 +61,9 @@ s.bind((HOST,PORT))
 s.listen()
 while True:
     conn,addr=s.accept()
+    Start_new_math_thread(conn,addr)
 
-
+s.close()
 
 
 
