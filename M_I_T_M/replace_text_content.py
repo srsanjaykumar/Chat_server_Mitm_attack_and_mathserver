1f7cd567f06d4f0f1887e4ebce917cde272ed214
sanjay <sanjaykumarravikumar1020@gmail.com>
2023-06-29 15:46:07 +0530

    replace a content by using mitm attack at particular web request

---
+++ M_I_T_M/replace_text_content.py
@@ -0,0 +1,22 @@
+from mitmproxy import http
+
+
+def response(flow: http.HTTPFlow) -> None:
+    # if flow.request.pretty_url == "https://example.com/path":
+    #     flow.response = http.HTTPResponse.make(
+    #         200, # (optional) status code
+    #         b"Hello World" , # (optional) content
+    #         {"Content-Type" : "text/html"} # ( optional) content
+    #     )
+    if flow.request.pretty_url == "testphp.vulnweb.com":
+        # b is byte  or other wise you put .encode()
+        # text we convert into number
+        flow.response.content=flow.response.content.replace(b"artists",b"sanjaykumar")
+        flow.response.content = flow.response.content.replace(b"Apache2", b"Pulsar2")
+
+
+
+
+# run $: mitmproxy -s filename.py
+
+#  after you find any error : run  in mitmproxy : console.view.eventlog
