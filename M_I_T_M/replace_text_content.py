e257d1407914901b6748a5f9d560cb36c5f5dce7
sanjay <sanjaykumarravikumar1020@gmail.com>
2023-06-29 20:10:43 +0530

    change name is working or not

--- M_I_T_M/replace_text_content.py
+++ M_I_T_M/replace_text_content.py
@@ -11,6 +11,7 @@ def response(flow: http.HTTPFlow) -> None:
     if flow.request.pretty_url == "testphp.vulnweb.com":
         # b is byte  or other wise you put .encode()
         # text we convert into number
+        # To add n number of changes 
         flow.response.content=flow.response.content.replace(b"artists",b"sanjaykumar")
         flow.response.content = flow.response.content.replace(b"Apache2", b"Pulsar2")
 
